dd88022d485de164b092e5e34fe0e9d51d78a3fc
balanayj <jbalanay.rose934@gmail.com>
2025-02-19 15:28:43 -0500

    Add security, events, listeners, handlers, and refactor with async await to script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+// Selectors
 const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("jokeBtn");
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
 
-  fetch("https://icanhazdadjoke.com", config)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
 }
 
 generateJoke();
+
+// Listeners
+jokeBtn.addEventListener("click", generateJoke);
